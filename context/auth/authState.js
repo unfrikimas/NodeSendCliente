f20dc3f06a8b981aa3226fdb89dd9a12e95ac516
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-01 19:08:34 +0100

    Creando el state para el context

--- context/auth/authState.js
+++ context/auth/authState.js
@@ -1,12 +1,27 @@
 import React, { useReducer } from 'react';
 import authContext from './authContext';
+import authReducer from './authReducer';
 
 const AuthState = ({children}) => {
+
+    //definir el state inicial
+    const initialState = {
+        token: '',
+        autenticado: null,
+        usuario: null,
+        mensaje: null
+    }
+
+    //definir el reducer
+    const [ state, dispatch ] = useReducer(authReducer, initialState);
 
     return (
         <authContext.Provider
             value={{
-                
+                token: state.token,
+                autenticado: state.autenticado,
+                usuario: state.usuario,
+                mensaje: state.mensaje
             }}
         >
             {children}
