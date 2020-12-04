0c6b365f44c589e23a9fcb4288704ce87583c421
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-04 18:03:38 +0100

    Mostrando un formulario para el password

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Formulario = () => {
+
+    const [ tienePassword, setTienePassword ] = useState(false);
+
     return (  
         <div className="w-full mt-10">
             <label className="text-lg text-gray-800">Eliminar después de:</label>
@@ -12,6 +15,22 @@ const Formulario = () => {
                     <option value="10">10 Descargas</option>
                     <option value="20">20 Descargas</option>
                 </select>
+            </div>
+            <div className="mt-4">
+                <div className="flex justify-between items-center">
+                    <label className="text-lg text-gray-800 mr-2">Proteger con contraseña</label>
+                    <input 
+                        type="checkbox"
+                        onChange={ () => setTienePassword(!tienePassword) }
+                    />
+                </div>
+                { tienePassword ?
+                    <input 
+                        type="password"
+                        className="appearance-none w-full mt-2 bg-white border border-gray-400 text-black py-3 px-4 pr-8 rounded leading-none focus:outline-none focus:border-gray-500" 
+                    />
+                : null }
+
             </div>
         </div>
     );
