9fc7c9eb49671fcf88ad7e17d371984c3fb19e13
Carlos Enrique Herrera <carlos@unfrikimas.com>
2020-12-07 12:14:34 +0100

    Variables de entorno

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
         backendURL: 'https://powerful-shelf-30692.herokuapp.com/',
-        frontendURL: 'https://nodesend-cliente.unfrikimas.vercel.app/'
+        frontendURL: 'https://nodesend-cliente-one.vercel.app/'
     }
 }
